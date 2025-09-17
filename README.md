a4c710cd62f4f74af80c1a037f31aa68d7f01a71
B0T-Josh <jbsantos18.jbs@gmail.com>
2025-09-17 12:25:46 +0800

    Edited error message

--- README.md
+++ README.md
@@ -19,26 +19,33 @@
 
 ### Syntax
         update [command] [option]
+
 ### Command 
-        - ['-a', '-c', '-p', '-f', '-m', '-A']
+        ['-a', '-c', '-p', '-f', '-m', '-A', '-P']
+
 ### Option 
-        -a is chosen, type the name of the file that you wanted to add changes into.
-        -c is chosen, type the message for the commit.
-        -p is chosen, you need to type the branch to where you will push your work. This pushes your updates to the remote branch
-        -f is chosen, you need to type the branch that you want to fetch. This fetches updates from the remote branch
-        -m is chosen, you need to type the branch that you want to merge with. This merges your local repository with the updates from the remote branch
-        -A is chosen, you don't need to type anything after it. This will fetch, merge, add changes, commit and push with one command
+        - -a - type the name of the file that you wanted to add changes into.
+        - -c - type the message for the commit.
+        - -p - you need to type the branch to where you will push your work. This pushes your updates to the remote branch.
+        - -f - you need to type the branch that you want to fetch. This fetches updates from the remote branch.
+        - -m - you need to type the branch that you want to merge with. This merges your local repository with the updates from the remote branch.
+        - -A - you don't need to type anything after it. This will fetch, merge, add changes, commit and push with one command.
+        - -P - you need to type the branch that you want to fetch and merge. This fetches updates and merges it from the remote branch to your local repository.
+
 ### Usage:
-        update -a [filename] / . (to add all changes)
-        update -a [filename] / . (to add all changes)
-        update -c [comment/message]
-        update -p [branch]
-        update -f [branch]
-        update -m [branch]
+        - update -a [filename] / . (to add all changes)
+        - update -c [comment/message]
+        - update -p [branch]
+        - update -f [branch]
+        - update -m [branch]
+        - update -A [branch]
+        - update -P [branch]
+
 ### Proper usage:
-        update -f -m
-        update -a [filename/.] -c [comment/message] -p [branch]
-        update -A
+        - update -f [branch] -m [branch]
+        - update -a [filename/.] -c [comment/message] -p [branch]
+        - update -A [branch]
+        - update -P [branch]
 
 #### Important note:
 Make sure to fetch and merge before you work on any file.<br>
--- update.c
+++ update.c
@@ -45,12 +45,12 @@ bool commit(char *comment) {
     } else return false;
 }
 
-bool updateAll() {
-    if(fetch("main")) {
-        if(merge("main")) {
+bool updateAll(char *branch) {
+    if(fetch(branch)) {
+        if(merge(branch)) {
             if(addChange(".")) {
                 if(commit("Update")) {
-                    if(upload("main")) {
+                    if(upload(branch)) {
                         return true;
                     }
                 }
@@ -61,24 +61,28 @@ bool updateAll() {
 }
 
 void printError() {
-    printf("Syntax:\nupdate <command> <option>\n"); 
-    printf("command - ['-a', '-c', '-p', '-f', '-m', '-A']\n");
-    printf("option - if -a is chosen, type the name of the file that you wanted to add changes into.\n");
-    printf("       - if -c is chosen, type the message for the commit.\n");
-    printf("       - if -p is chosen, you need to type the branch to where you will push your work. This pushes your updates to the remote branch\n");
-    printf("       - if -f is chosen, you don't need to type anything after it. This fetches updates from the remote branch\n");
-    printf("       - if -m is chosen, you need to type the branch that you want to merge with. This merges your local repository with the updates from the remote branch\n");
-    printf("       - if -A is chosen, you don't need to type anything after it. This will fetch, merge, add changes, commit and push with one command\n");
-    printf("usage:\nupdate -a <filename> / . (to add all changes)\n");
-    printf("update -a <filename> / . (to add all changes)\n");
-    printf("update -c <comment/message>\n");
-    printf("update -p <branch>\n");
-    printf("update -f <branch>\n");
-    printf("update -m <branch>\n");
-    printf("proper usage:\n");
-    printf("update -f <branch> -m <branch>\n");
-    printf("update -a <filename/.> -c <comment/message> -p <branch>\n");
-    printf("update -A\n");
+    printf("Syntax:\n\tupdate [command] [option]\n"); 
+    printf("Command\n\t['-a', '-c', '-p', '-f', '-m', '-A']\n");
+    printf("Option\n\t- -a - type the name of the file that you wanted to add changes into.\n\t");
+    printf("- -c - type the message for the commit.\n\t");
+    printf("- -p - you need to type the branch to where you will push your work. This pushes your updates to the remote branch.\n\t");
+    printf("- -f - you need to type the branch that you want to fetch. This fetches updates from the remote branch.\n\t");
+    printf("- -m - you need to type the branch that you want to merge with. This merges your local repository with the updates from the remote branch.\n\t");
+    printf("- -A - you don't need to type anything after it. This will fetch, merge, add changes, commit and push with one command.\n\t");
+    printf("- -P - you need to type the branch that you want to fetch and merge. This fetches updates and merges it from the remote branch to your local repository.\n");
+    printf("Usage:\n\tupdate -a <filename> / . (to add all changes)\n\t");
+    printf("- update -a [filename] / . (to add all changes)\n\t");
+    printf("- update -c [comment/message]\n\t");
+    printf("- update -p [branch]\n\t");
+    printf("- update -f [branch]\n\t");
+    printf("- update -m [branch]\n\t");
+    printf("- update -A [branch]\n\t");
+    printf("- update -P [branch]\n");
+    printf("Proper usage:\n\t");
+    printf("- update -f [branch] -m [branch]\n\t");
+    printf("- update -a [filename/.] -c [comment/message] -p [branch]\n\t");
+    printf("- update -A [branch]\n\t");
+    printf("- update -P [branch]\n");
     printf("\nImportant note:\nMake sure to fetch and merge before you work on any file.\n");
     printf("Push everytime you finish a file\n");
 }
@@ -127,7 +131,7 @@ int start(int argc, char *argv[]) {
                     return 0;
                 }
             } else if(strncmp(argv[i], "-A", 2) == 0) {
-                if(updateAll()) {
+                if(updateAll(argv[i+1])) {
                     printf("Update all successful\n");
                 } else {
                     printf("Failed to update all\n"); 
