7eadf0dc3484f859df184b27707e740321be9254
B0T-Josh <jbsantos18.jbs@gmail.com>
2026-02-10 11:12:44 +0800

    added

--- GitUpdate2/update/obj/Debug/net8.0/update.AssemblyInfo.cs
+++ GitUpdate2/update/obj/Debug/net8.0/update.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("update")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4eb60131f0a8d2275d63f0cfb526066d7e8438")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27bff6d2817e5cb887e78265aa107a1acc7b769")]
 [assembly: System.Reflection.AssemblyProductAttribute("update")]
 [assembly: System.Reflection.AssemblyTitleAttribute("update")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
